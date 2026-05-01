84868605064aa8e9ebcdc81970dc8ea13cd10ea5
Ansel Joseph <anseljoseph2509@gmail.com>
2026-05-01 14:22:51 +0530

    add: repeat user input

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from voice import speak
+from voice import speak, listen
 
 def main():
     speak("Hello Ansel. I am Timothy.")
+
+    while True:
+        user_input = listen()
+
+        if user_input == "exit":
+            speak("Goodbye.")
+            break
+
+        speak(f"You said {user_input}")
 
 if __name__ == "__main__":
     main()
